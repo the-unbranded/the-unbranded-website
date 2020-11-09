3047f0a50a5c84ece505adfc5a3b1f495eca76ed
jennc <jcameron2486@gmail.com>
2020-11-09 16:56:56 -0600

    Incorporated heather's changes to git

---
+++ script.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+
+     //Open hamburger menu on click
+    $('.menu-toggle').click(function() {
+        $('.nav').toggleClass('nav--open');
+        $(this).toggleClass('open');
+        $('.nav').show();
+        
+    });
+    
+
+    //hide menu when nav list item is clicked
+    $('.nav li').click(function() {
+        $('.nav').toggleClass('nav--open');
+        $('.menu-toggle').toggleClass('open');
+        
+    });
+    
+});
+
+
+$(window).scroll(function() {  //update this so that the logo stops fading at about 30%
+    var scrollTop = $(this).scrollTop();
+
+        $('.header__logo__img').css({
+        opacity: function() {
+            var elementHeight = $(this).height(),
+            opacity = ((elementHeight - scrollTop + 1200) / elementHeight);
+            return opacity;
+        }
+    
+    
+});
+
+
+
+        
+    });
+    
